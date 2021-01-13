f6890ac2b4548c374800be1f583e1b3f0bf4ac0f
Ali Akman <thealico@gmail.com>
2021-01-14 00:34:10 +0300

    d

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `stock`,
+        path: `${__dirname}/src/stock`,
+      },
+    },
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-
-// import Layout from "../components/layout"
-// import Image from "../components/image"
-// import SEO from "../components/seo"
 
 const IndexPage = () => (
-
-  <h1>Albüm</h1>
   
-  <ol>
-    <li></li>
-  </ol>
+  <h1>Test</h1>
+  <ul>
+     <li>hey</li>
+  </ul>
+
 )
 
 export default IndexPage
